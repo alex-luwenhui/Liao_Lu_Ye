24c28c0a7085366ba6b04d11d0a5941dfbf743a2
LAPTOP-LNR9KB7E\Alex Lu <alex.luwenhui@utexas.edu>
2017-03-05 17:21:06 -0600

    try again

--- Lu_Liao_Ye/Lu_Liao_Ye/Models/Client.cs
+++ Lu_Liao_Ye/Lu_Liao_Ye/Models/Client.cs
@@ -2,18 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
 
 namespace Lu_Liao_Ye.Models
 {
     public class Client
     {
-        [Required]
         public int ClientID { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-
-
 
 
     }
