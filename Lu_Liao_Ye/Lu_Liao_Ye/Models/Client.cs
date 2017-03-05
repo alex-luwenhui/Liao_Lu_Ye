23094c89156e153e3cfa98887735a5b359c2a440
LAPTOP-IEOMOJGG\Ningxin Liao <ningxin.liao@utexas.edu>
2017-03-05 16:17:17 -0600

    added attributes to client.cs & clientinfo.cs

--- Lu_Liao_Ye/Lu_Liao_Ye/Models/Client.cs
+++ Lu_Liao_Ye/Lu_Liao_Ye/Models/Client.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lu_Liao_Ye.Models
 {
     public class Client
     {
+        [Required]
         public int ClientID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
 
 
     }
---
+++ Lu_Liao_Ye/Lu_Liao_Ye/Models/ClientInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Lu_Liao_Ye.Models
+{
+    public class ClientInfo
+    {
+        [Display(Name = "Client Info ID")]
+        public int ClientInfoID { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Enter a valid email address.")]
+        [Display(Name = "Email Address")]
+        [EmailAddress (ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
+
+
+        public string DOB { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set;}
+
+
+
+    }
+}
