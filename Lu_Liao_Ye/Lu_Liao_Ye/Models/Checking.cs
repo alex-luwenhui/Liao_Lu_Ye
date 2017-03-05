17fadd9ba95a0467291e372ee42aa72534e022aa
LAPTOP-LNR9KB7E\Alex Lu <alex.luwenhui@utexas.edu>
2017-03-05 17:30:23 -0600

    All of Alex's edits

---
+++ Lu_Liao_Ye/Lu_Liao_Ye/Models/Checking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lu_Liao_Ye.Models
+{
+    public class Checking
+    {
+        public int CheckingID { get; set; }
+
+        [Required]
+        public string AccountName { get; set; } = "Longhorn Checking";
+
+        [Required]
+        public int AccountNumber { get; set; }
+
+        [Required]
+        public decimal Balance { get; set; } = 0;
+    }
+}
---
+++ Lu_Liao_Ye/Lu_Liao_Ye/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lu_Liao_Ye.Models
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+
+
+    }
+}
